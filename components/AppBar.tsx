c20303ff26b1dfd47da5ab9d2f89dae28e89d044
ThalusA <alexandre.monierc@gmail.com>
2021-12-07 14:21:01 +0100

    Add basic theme and simple app bar

---
+++ components/AppBar.tsx
@@ -0,0 +1,83 @@
+import Image from 'next/image'
+import { styled, alpha } from '@mui/material/styles';
+import MUIAppBar from '@mui/material/AppBar';
+import { useRouter } from 'next/router'
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { NextPage } from 'next';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '30ch',
+            '&:focus': {
+                width: '40ch',
+            },
+        },
+    },
+}));
+
+const AppBar: NextPage = (props) => {
+    const router = useRouter();
+
+    return (
+        <MUIAppBar position="fixed" {...props}>
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="back home"
+                    sx={{ mr: 2 }}
+                    onClick={() => router.push('/')}
+                >
+                    <Image src="/sublogo.svg" height={42} width={42} alt="ResearchShare logo" />
+                </IconButton>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+            </Toolbar>
+        </MUIAppBar>
+    )
+};
+
+export default AppBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { common, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: purple[400]
+        },
+        secondary: {
+            main: common.white
+        },
+    }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
