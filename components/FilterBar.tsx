733f74c66879f7bb6e97f7639a7ddf86f3f510e2
jeremy.braconnier <jerem@pop-os.localdomain>
2021-12-09 16:26:53 +0100

    better handling of the filter

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -6,108 +6,134 @@ import Slider from '@mui/material/Slider';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
+import { category, price } from '../jsonFiles/filterCategory.json';
 
 const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  marginTop: -10,
-  border: '3px solid #7447D5',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.grey[400], 0.15),
-  '&:hover': {
-      backgroundColor: alpha(theme.palette.grey[400], 0.25),
-  },
-  marginLeft: 0,
+    position: 'relative',
+    marginTop: '0.625rem',
+    border: '3px solid #7447D5',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.grey[400], 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.grey[400], 0.25),
+    },
+    marginLeft: 0,
 
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-  },
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-      padding: theme.spacing(0, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-          width: '30ch',
-          '&:focus': {
-              width: '40ch',
-          },
-      },
-  },
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(0, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '30ch',
+            '&:focus': {
+                width: '40ch',
+            },
+        },
+    },
 }));
 
-
-/*
-TODO: Take a json and display all categories from this json
-*/
 const FilterBar: NextPage = () => {
-  return(
-    <div className="FlexboxContainer">
-      <div className="FilterBar">
-        <h4 className="Label">Filters</h4>
-        <div className="Input">
-          <Search>
-            <SearchIconWrapper>
-                <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-                placeholder="Search for filter…"
-                inputProps={{ 'aria-label': 'search' }}
-            />
-        </Search>
-      </div>
-      <h4 className="Label">Date</h4>
-      <div className="Input">
-        <Slider defaultValue={1950} aria-label="Default" valueLabelDisplay="auto" min={1990} max={2021} color="secondary"/>
-      </div>
-      <h4 className="Label">Category</h4>
-      <div className="Input">
-        <FormGroup>
-          <FormControlLabel control={<Checkbox color="secondary"/>} label="Astronomy"/>
-          <FormControlLabel control={<Checkbox color="secondary"/>} label="Physics"/>
-          <FormControlLabel control={<Checkbox color="secondary"/>} label="Sociology"/>
-        </FormGroup>
-      </div>
-      <h4 className="Label">Price</h4>
-      <div className="Input">
-        <FormGroup>
-          <FormControlLabel control={<Checkbox color="secondary"/>} label="Free"/>
-          <FormControlLabel control={<Checkbox color="secondary"/>} label="Paid"/>
-        </FormGroup>
-      </div>
-      </div>
-      <style jsx>{`
-        .FilterBar {
-          background-color: rgba(237, 237, 237, 0.6);
-          font-size: 22px;
-        }
-        .Input {
-          margin-top: -10px;
-          padding-left: 30px;
-          padding-right: 30px;
-        }
-        .Label {
-          padding-left: 10px;
-        }
-      `}</style>
-    </div>
-  );
+    return(
+        <Box>
+        <Box sx={{
+                    bgcolor: 'rgba(237, 237, 237, 0.6)'
+        }}>
+            <Typography variant="h4" sx={{
+            paddingLeft: '0.625rem',
+            fontWeight: 1000,
+            fontSize: '22px',
+            }}>Filters</Typography>
+            <Box sx={{
+                    marginTop: '0.625rem',
+                    marginBottom: '1.25rem',
+                    paddingLeft: '1.875rem',
+                    paddingRight: '1.875rem',
+            }}>
+            <Search>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase sx={{
+                marginTop: '0.625rem',
+                }}
+                    placeholder="Search for filter…"
+                    inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+        </Box>
+        <Typography variant="h4" sx={{
+            paddingLeft: '0.625rem',
+            fontWeight: 1000,
+            fontSize: '22px',
+            }}>Date</Typography>
+        <Box sx={{
+                    marginTop: '0.625rem',
+                    marginBottom: '1.25rem',
+                    paddingLeft: '1.875rem',
+                    paddingRight: '1.875rem',
+            }}>
+            <Slider defaultValue={1950} aria-label="Default" valueLabelDisplay="auto" min={1990} max={2021} color="secondary"/>
+        </Box>
+        <Typography variant="h4" sx={{
+            paddingLeft: '0.625rem',
+            fontWeight: 1000,
+            fontSize: '22px',
+            }}>Category</Typography>
+        <Box sx={{
+                    marginTop: '0.625rem',
+                    marginBottom: '1.25rem',
+                    paddingLeft: '1.875rem',
+                    paddingRight: '1.875rem',
+            }}>
+            <FormGroup>
+            {category.map(name => (
+                <FormControlLabel control={<Checkbox color="secondary"/>} label={name} key={name}/>
+            ))}
+            </FormGroup>
+        </Box>
+        <Typography variant="h4" sx={{
+            paddingLeft: '0.625rem',
+            fontWeight: 1000,
+            fontSize: '22px',
+            }}>Price</Typography>
+        <Box sx={{
+                    marginTop: '0.625rem',
+                    marginBottom: '1.25rem',
+                    paddingLeft: '1.875rem',
+                    paddingRight: '1.875rem',
+            }}>
+            <FormGroup>
+            {price.map(name => (
+                <FormControlLabel control={<Checkbox color="secondary"/>} label={name} key={name}/>
+            ))}
+            </FormGroup>
+        </Box>
+        </Box>
+        </Box>
+    );
 }
 
 export default FilterBar;
