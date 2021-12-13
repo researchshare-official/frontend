f0095965645c8a39177f40ca599489b9bc353638
jeremy.braconnier <jerem@pop-os.localdomain>
2021-12-13 11:23:24 +0100

    adding login form

--- components/Login.tsx
+++ components/Login.tsx
@@ -7,7 +7,7 @@ import { Divider } from '@mui/material';
 
 const useStyles = makeStyles((theme: Theme) => ({
     backDrop: {
-      backdropFilter: "blur(0.01rem)",
+      backdropFilter: "blur(0.1rem)",
       backgroundColor:'rgba(0,0,30,0.4)'
     },
     notchedOutline: {
@@ -28,11 +28,14 @@ const Login: NextPage = () => {
             root: classes.backDrop,
             },
         }}
+        PaperProps={{
+            style: { borderRadius: '10px' }
+          }}
         onClose={() => {}}
         >
-        <DialogContent style={{ width: '30rem', marginLeft: '1rem'}}>
-            <Typography variant="body1" sx={{color: '#7447D5', marginBottom: '12px'}}>Login</Typography>
-            <TextField id="login" variant="outlined" focused sx={{marginBottom: '30px', width: '25rem'}}
+        <DialogContent style={{ width: '30rem', backgroundColor: 'white'}}>
+            <Typography variant="body1" sx={{color: '#7447D5', marginBottom: '12px', fontWeight: 'bold'}}>Login</Typography>
+            <TextField id="login" variant="outlined" focused sx={{marginBottom: '30px', width: '26.5rem'}}
             InputProps={{
                 classes: {
                   notchedOutline: classes.notchedOutline
@@ -40,8 +43,8 @@ const Login: NextPage = () => {
               }}
             className={classes.backgroundForm}
             />
-            <Typography variant="body1" sx={{color: '#7447D5', marginBottom: '12px'}}>Password</Typography>
-            <TextField id="password" variant="outlined" focused type="password" sx={{width: '25rem'}}
+            <Typography variant="body1" sx={{color: '#7447D5', marginBottom: '12px', fontWeight: 'bold'}}>Password</Typography>
+            <TextField id="password" variant="outlined" focused type="password" sx={{width: '26.5rem'}}
             InputProps={{
                 classes: {
                   notchedOutline: classes.notchedOutline
@@ -49,13 +52,13 @@ const Login: NextPage = () => {
               }}
             className={classes.backgroundForm}
             />
-            <Typography variant="caption" sx={{color: '#7447D5', textDecoration: 'underline', marginBottom: '30px', marginTop: '17px', display: 'flex', flexDirection: 'row-reverse', marginRight: '2rem'}}>Forgot password</Typography>
+            <Typography variant="caption" sx={{color: '#7447D5', textDecoration: 'underline', marginBottom: '30px', marginTop: '17px', display: 'flex', flexDirection: 'row-reverse', marginRight: '0.8rem'}}>Forgot password</Typography>
             <Box sx={{alignItems: 'center', justifyContent: 'center', display:'flex'}}>
-                <Button variant="contained" sx={{marginBottom: '30px', backgroundColor: '#7447D5', textTransform: 'none', fontWeight: 'bold', '&:hover': { color: "primary.main", bgcolor: 'secondary.dark' }}}>Login</Button>
+                <Button variant="contained" sx={{borderRadius: '10px', width: '10rem', marginBottom: '30px', backgroundColor: '#7447D5', color: 'white', textTransform: 'none', fontWeight: 'bold', '&:hover': { color: "primary.main", bgcolor: 'secondary.dark' }}}>Login</Button>
             </Box>
             <Divider variant="middle" sx={{marginBottom: '30px'}}/>
             <Box sx={{alignItems: 'center', justifyContent: 'center', display:'flex'}}>
-                <Button variant="contained" sx={{marginBottom: '30px',backgroundColor: '#7447D5', textTransform: 'none', fontWeight: 'bold'}}>Create an account</Button>
+                <Button variant="contained" sx={{borderRadius: '10px', backgroundColor: '#7447D5', color: 'white', textTransform: 'none', fontWeight: 'bold', '&:hover': { color: "primary.main", bgcolor: 'secondary.dark' }}}>Create an account</Button>
             </Box>
         </DialogContent>
         </Dialog>
