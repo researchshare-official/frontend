f1ebd86bbac896893bcecc89db1325245214b2aa
Falaxir <falaxir@gmail.com>
2021-12-16 14:11:06 +0100

    corrected es lints

--- components/DetailedSearch/CategoriesFilter.tsx
+++ components/DetailedSearch/CategoriesFilter.tsx
@@ -14,94 +14,94 @@ import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 
 const CategoriesFilter = () => {
-  return (
-    <Box sx={{ display: "flex", flexDirection: "column", width: 1 / 1 }}>
-      <Typography variant="h5" sx={{ marginTop: "2rem", marginLeft: "8%" }}>
-        {" "}
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", width: 1 / 1 }}>
+            <Typography variant="h5" sx={{ marginTop: "2rem", marginLeft: "8%" }}>
+                {" "}
         Categories{" "}
-      </Typography>
-      <Box>
-        <Button
-          variant="contained"
-          sx={{
-            width: "10rem",
-            marginLeft: "8%",
-            color: "primary.main",
-            bgcolor: "secondary.main",
-            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
-          }}
-        >
-          <Typography sx={{ textTransform: "capitalize" }}>
+            </Typography>
+            <Box>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "10rem",
+                        marginLeft: "8%",
+                        color: "primary.main",
+                        bgcolor: "secondary.main",
+                        "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+                    }}
+                >
+                    <Typography sx={{ textTransform: "capitalize" }}>
             2021-March
-          </Typography>
-        </Button>
-      </Box>
-      <Box sx={{ marginTop: "3%" }}>
-        <Button
-          variant="contained"
-          sx={{
-            width: "10rem",
-            marginLeft: "16%",
-            color: "secondary.main",
-            bgcolor: "white",
-            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
-          }}
-        >
-          <Typography sx={{ textTransform: "capitalize" }}>
+                    </Typography>
+                </Button>
+            </Box>
+            <Box sx={{ marginTop: "3%" }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "10rem",
+                        marginLeft: "16%",
+                        color: "secondary.main",
+                        bgcolor: "white",
+                        "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+                    }}
+                >
+                    <Typography sx={{ textTransform: "capitalize" }}>
             The impact...
-          </Typography>
-        </Button>
-      </Box>
-      <Box sx={{ marginTop: "3%" }}>
-        <Button
-          variant="contained"
-          sx={{
-            width: "10rem",
-            marginLeft: "16%",
-            color: "secondary.main",
-            bgcolor: "white",
-            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
-          }}
-        >
-          <Typography sx={{ textTransform: "capitalize" }}>
+                    </Typography>
+                </Button>
+            </Box>
+            <Box sx={{ marginTop: "3%" }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "10rem",
+                        marginLeft: "16%",
+                        color: "secondary.main",
+                        bgcolor: "white",
+                        "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+                    }}
+                >
+                    <Typography sx={{ textTransform: "capitalize" }}>
             Climate ch...
-          </Typography>
-        </Button>
-      </Box>
-      <Box sx={{ marginTop: "3%" }}>
-        <Button
-          variant="contained"
-          sx={{
-            width: "10rem",
-            marginLeft: "8%",
-            color: "secondary.main",
-            bgcolor: "white",
-            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
-          }}
-        >
-          <Typography sx={{ textTransform: "capitalize" }}>
+                    </Typography>
+                </Button>
+            </Box>
+            <Box sx={{ marginTop: "3%" }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "10rem",
+                        marginLeft: "8%",
+                        color: "secondary.main",
+                        bgcolor: "white",
+                        "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+                    }}
+                >
+                    <Typography sx={{ textTransform: "capitalize" }}>
             2021-November
-          </Typography>
-        </Button>
-      </Box>
-      <Box sx={{ marginTop: "3%" }}>
-        <Button
-          variant="contained"
-          sx={{
-            width: "10rem",
-            marginLeft: "8%",
-            color: "secondary.main",
-            bgcolor: "white",
-            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
-          }}
-        >
-          <Typography sx={{ textTransform: "capitalize" }}>
+                    </Typography>
+                </Button>
+            </Box>
+            <Box sx={{ marginTop: "3%" }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "10rem",
+                        marginLeft: "8%",
+                        color: "secondary.main",
+                        bgcolor: "white",
+                        "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+                    }}
+                >
+                    <Typography sx={{ textTransform: "capitalize" }}>
             Other Dates
-          </Typography>
-        </Button>
-      </Box>
-    </Box>
-  );
+                    </Typography>
+                </Button>
+            </Box>
+        </Box>
+    );
 };
 
 export default CategoriesFilter;
--- components/DetailedSearch/SideBar.tsx
+++ components/DetailedSearch/SideBar.tsx
@@ -5,29 +5,29 @@ import IncludeFilter from "./IncludeFilter";
 import CategoriesFilter from "./CategoriesFilter";
 
 const SideBar = () => {
-  return (
-    <Box sx={{ display: "flex", flexDirection: "row", width: 1 / 5 }}>
-      <Box sx={{ display: "flex", flexDirection: "column", width: 1 / 1 }}>
-        <WhatAreYouLookingFor></WhatAreYouLookingFor>
-        <Date></Date>
-        <IncludeFilter></IncludeFilter>
-        <CategoriesFilter></CategoriesFilter>
-      </Box>
-      <Box
-        sx={{
-          display: "flex-end",
-          flexDirection: "row-reverse",
-          marginLeft: "auto",
-        }}
-      >
-        <Divider
-          variant="fullWidth"
-          orientation="vertical"
-          sx={{ marginTop: "2rem", alignSelf: "right" }}
-        />
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: "flex", flexDirection: "row", width: 1 / 5 }}>
+            <Box sx={{ display: "flex", flexDirection: "column", width: 1 / 1 }}>
+                <WhatAreYouLookingFor></WhatAreYouLookingFor>
+                <Date></Date>
+                <IncludeFilter></IncludeFilter>
+                <CategoriesFilter></CategoriesFilter>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex-end",
+                    flexDirection: "row-reverse",
+                    marginLeft: "auto",
+                }}
+            >
+                <Divider
+                    variant="fullWidth"
+                    orientation="vertical"
+                    sx={{ marginTop: "2rem", alignSelf: "right" }}
+                />
+            </Box>
+        </Box>
+    );
 };
 
 export default SideBar;
