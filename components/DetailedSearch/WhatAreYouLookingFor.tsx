bef44fc18e2c66e7b8faba8a24add9a97779facd
Jeremy B <jeremy.braconnier@epitech.eu>
2021-12-16 11:18:53 +0100

    adding tabulation

--- components/DetailedSearch/WhatAreYouLookingFor.tsx
+++ components/DetailedSearch/WhatAreYouLookingFor.tsx
@@ -1,27 +1,44 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
 const WhatAreYouLookingFor = () => {
-    return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', width: 1/1}}>
-            <Typography variant="h5" sx={{marginTop:'2rem', marginLeft:'8%',}}> What are you looking for </Typography>
-            <Box>
-            <Button variant="contained"
-            sx={{ width: '8rem', marginLeft:'8%', color: "primary.main", bgcolor: "secondary.main", 
-            '&:hover': { color: "primary.main", bgcolor: 'secondary.dark'}}}>
-                <Typography sx={{ textTransform: 'capitalize' }}>Articles</Typography>
-            </Button>
-            </Box>
-            <Box sx={{marginTop: '3%'}}>
-            <Button variant="contained"
-            sx={{ width: '8rem', marginLeft:'8%', color: "secondary.main", bgcolor: "white", 
-            '&:hover': { color: "primary.main", bgcolor: 'secondary.dark'}}}>
-                <Typography sx={{ textTransform: 'capitalize' }}>Authors</Typography>
-            </Button>
-            </Box>
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", width: 1 / 1 }}>
+      <Typography variant="h5" sx={{ marginTop: "2rem", marginLeft: "8%" }}>
+        {" "}
+        What are you looking for{" "}
+      </Typography>
+      <Box>
+        <Button
+          variant="contained"
+          sx={{
+            width: "8rem",
+            marginLeft: "8%",
+            color: "primary.main",
+            bgcolor: "secondary.main",
+            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+          }}
+        >
+          <Typography sx={{ textTransform: "capitalize" }}>Articles</Typography>
+        </Button>
+      </Box>
+      <Box sx={{ marginTop: "3%" }}>
+        <Button
+          variant="contained"
+          sx={{
+            width: "8rem",
+            marginLeft: "8%",
+            color: "secondary.main",
+            bgcolor: "white",
+            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+          }}
+        >
+          <Typography sx={{ textTransform: "capitalize" }}>Authors</Typography>
+        </Button>
+      </Box>
     </Box>
-    )
+  );
 };
 
 export default WhatAreYouLookingFor;
