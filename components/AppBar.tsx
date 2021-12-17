1efe1fc316ff16bb6f646ac61ead779f529699e3
Jeremy B <jeremy.braconnier@epitech.eu>
2021-12-17 09:47:19 +0100

    fix linter problem

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,97 +1,110 @@
-import Image from 'next/image'
-import { styled, alpha, useTheme } from '@mui/material/styles';
-import MUIAppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import { useRouter } from 'next/router'
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import { NextPage } from 'next';
-import { useContext } from 'react';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import Image from "next/image";
+import { styled, alpha, useTheme } from "@mui/material/styles";
+import MUIAppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import { useRouter } from "next/router";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import { NextPage } from "next";
+import { useContext } from "react";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.grey[400], 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.grey[400], 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    border: 'solid',
-    borderColor: theme.palette.secondary.main,
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.grey[400], 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.grey[400], 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  border: "solid",
+  borderColor: theme.palette.secondary.main,
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '25ch',
-            '&:focus': {
-                width: '40ch',
-            },
-        },
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "25ch",
+      "&:focus": {
+        width: "40ch",
+      },
     },
+  },
 }));
 
 const AppBar: NextPage = (props) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <MUIAppBar position="fixed" {...props}>
-            <Toolbar disableGutters={true} sx={{ px: '20%' }}>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="back home"
-                    sx={{ mr: 2 }}
-                    onClick={() => router.push('/')}
-                >
-                    <Image src="/sublogo.svg" height={42} width={42} alt="ResearchShare logo" />
-                </IconButton>
-                <Divider orientation="vertical" flexItem={true} />
-                <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon />
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                        placeholder="Search…"
-                        inputProps={{ 'aria-label': 'search' }}
-                    />
-                </Search>
+  return (
+    <MUIAppBar position="fixed" {...props}>
+      <Toolbar disableGutters={true} sx={{ px: "20%" }}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="back home"
+          sx={{ mr: 2 }}
+          onClick={() => router.push("/")}
+        >
+          <Image
+            src="/sublogo.svg"
+            height={42}
+            width={42}
+            alt="ResearchShare logo"
+          />
+        </IconButton>
+        <Divider orientation="vertical" flexItem={true} />
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
 
-                <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ flexGrow: 1 }} />
 
-                <Button variant="contained" sx={{ width: '8rem', color: "primary.main", bgcolor: "secondary.main", '&:hover': { color: "primary.main", bgcolor: 'secondary.dark' }}}>
-                    <Typography sx={{ textTransform: 'capitalize' }}>Login</Typography>
-                </Button>
-            </Toolbar>
-        </MUIAppBar>
-    )
+        <Button
+          variant="contained"
+          sx={{
+            width: "8rem",
+            color: "primary.main",
+            bgcolor: "secondary.main",
+            "&:hover": { color: "primary.main", bgcolor: "secondary.dark" },
+          }}
+        >
+          <Typography sx={{ textTransform: "capitalize" }}>Login</Typography>
+        </Button>
+      </Toolbar>
+    </MUIAppBar>
+  );
 };
 
 export default AppBar;
